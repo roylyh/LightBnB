6b476f1af25181106077692ed0775476886d8f48
roylyh <roy.louyihui@gmail.com>
2023-03-09 21:47:35 -0800

    err handler modified

--- LightBnB_WebApp/server/apiRoutes.js
+++ LightBnB_WebApp/server/apiRoutes.js
@@ -2,25 +2,27 @@ module.exports = function(router, database) {
 
   router.get('/properties', (req, res) => {
     database.getAllProperties(req.query, 20)
-    .then(properties => res.send({properties}))
-    .catch(e => {
-      console.error(e);
-      res.send(e)
-    }); 
+      .then(properties => res.send({properties}))
+      .catch(e => {
+        console.error(e);
+        res.send(e);
+      });
   });
 
   router.get('/reservations', (req, res) => {
     const userId = req.session.userId;
+    console.log("userId ", userId);
     if (!userId) {
-      res.error("💩");
+      res.status(403);
+      res.send("💩");
       return;
     }
     database.getAllReservations(userId)
-    .then(reservations => res.send({reservations}))
-    .catch(e => {
-      console.error(e);
-      res.send(e)
-    });
+      .then(reservations => res.send({reservations}))
+      .catch(e => {
+        console.error(e);
+        res.send(e);
+      });
   });
 
   router.post('/properties', (req, res) => {
@@ -31,9 +33,9 @@ module.exports = function(router, database) {
       })
       .catch(e => {
         console.error(e);
-        res.send(e)
+        res.send(e);
       });
   });
 
   return router;
-}
+};
